18c81659ee0ff3fb372fd7de017d7d0f5c5dfea2
natanMoutinho <natan.nt.iff@gmail.com>
2022-05-08 19:28:23 -0300

    Ultimos ajustes

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -60,7 +60,7 @@ export function WidgetFrom() {
                 </>
             )}
             <footer className="text-xs text-neutral-400">
-                Feito com amor S2, by nataN <a className="underline underline-offset-2" href="https://github.com/natanMoutinho"> GITHUB </a>
+                Feito com ♥ pelo <a className="underline underline-offset-2" href="https://github.com/natanMoutinho"> nataN </a>
             </footer>
         </div>
     );
